8e0466c6d150f8a710d6ea102b16f9c89aa002b5
gulay-yusifli <yusifligulay06@gmail.com>
2025-11-30 11:48:44 +0400

    task

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import { useState } from "react"
+import { Link } from 'react-router-dom';
 
-const Card = ({product}) => {
-  return (
-      <div className='min-w-[300px] w-full p-2.5 border-gray-300 rounded-md shadow-md'>
-        <img className='w-full h-[250px] object-contain' src={product.images[0]} alt=""/>
-        <h1 className='text-sxl font-semibold mt-3'>{product.title}</h1>
-        <p className='text-sm my-4'>{product.description}</p>
-        <p className='text-red-600 font-medium'>{product.currency}{product.prices}</p>
-      </div>
-  )
+const Card = ({ product }) => {
+    const [count, setCount] = useState(0)
+
+    return (
+        <Link to={`/details/${product._id}`}>
+            <div className="relative min-w-[300px] w-full border border-gray-300 p-4 rounded-lg shadow-lg">
+                <img className="w-full h-[200px] object-contain" src={product.image ? product.image : product.images[0]} alt="" />
+                <h2 className="text-2xl font-semibold mt-3">{product.title}</h2>
+                <p className="my-4">{product.description}</p>
+                <p className="text-red-600 font-medium">{product.currency}{product.price}</p>
+                <div className="absolute -bottom-2 right-3 w-fit flex justify-center items-center gap-5 py-5">
+                    <button onClick={() => {
+                        setCount(prevState => prevState - 1)
+                    }} className="flex items-center justify-center size-10 bg-red-600 text-3xl text-white">-</button>
+                    <p className="text-2xl">{count}</p>
+                    <button onClick={() => {
+                        setCount(prevState => prevState + 1)
+                    }} className="flex items-center justify-center size-10 bg-green-600 text-3xl text-white">+</button>
+                </div>
+            </div>
+        </Link>
+    )
 }
 
 export default Card
